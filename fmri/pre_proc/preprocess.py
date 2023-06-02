e70a3ba421c051bd48ea8449468906ece34317d8
cnsimmons <claire.simmons112@gmail.com>
2023-06-02 17:00:19 -0400

    small updates to preprocessing script

--- fmri/pre_proc/preprocess.py
+++ fmri/pre_proc/preprocess.py
@@ -5,16 +5,17 @@ import subprocess
 from glob import glob as glob
 import pdb
 
+#command line options
+exp = sys.argv[1] #which experiment
+sub = sys.argv[2] #WHICH SUBJECT
 
-exp = sys.argv[1]
-sub = sys.argv[2]
 print(exp, sub, flush = True)
 
 
 #add exp to each item in sub_list
 #
 
-tasks = ['spaceloc', 'toolloc','loc']
+tasks = ['loc']
 
 ses = 1
 runs = [1,2,3]
@@ -23,7 +24,7 @@ study_dir = f'/lab_data/behrmannlab/vlad/{exp}'
 
 
 #instatiate sub dir
-sub_dir = f'{study_dir}/sub-{sub}/ses-{ses:02d}'
+sub_dir = f'{study_dir}/{sub}/ses-{ses:02d}'
 #make derivatives folder
 os.makedirs(f'{sub_dir}/derivatives', exist_ok=True)
 #make fsl folder
@@ -37,9 +38,9 @@ anat_file = glob(f'{sub_dir}/anat/*_T1w.nii.gz')[0]
 
 
 #check if deskulled brain exists, else make it
-if not os.path.exists(f'{sub_dir}/anat/sub-{sub}_ses-{ses:02d}_T1w_brain.nii.gz'):
+if not os.path.exists(f'{sub_dir}/anat/{sub}_ses-{ses:02d}_T1w_brain.nii.gz'):
     #deskull brain
-    bash_cmd = f'bet {anat_file} {sub_dir}/anat/sub-{sub}_ses-{ses:02d}_T1w_brain.nii.gz -R -B'
+    bash_cmd = f'bet {anat_file} {sub_dir}/anat/{sub}_ses-{ses:02d}_T1w_brain.nii.gz -R -B'
     subprocess.run(bash_cmd.split(),check = True)
 
 for task in tasks:
@@ -49,12 +50,12 @@ for task in tasks:
     #loop through runs and calculate motion outliers
     for run in runs:
         #if bold file exists
-        if os.path.exists(f'{sub_dir}/func/sub-{sub}_ses-{ses:02d}_task-{task}_run-0{run}_bold.nii.gz'):
+        if os.path.exists(f'{sub_dir}/func/{sub}_ses-{ses:02d}_task-{task}_run-0{run}_bold.nii.gz'):
             #create run directory
             os.makedirs(f'{sub_dir}/derivatives/fsl/{task}/run-0{run}', exist_ok=True)
 
             #calcualte motion outliers
-            bash_cmd = f'fsl_motion_outliers -i {sub_dir}/func/sub-{sub}_ses-{ses:02d}_task-{task}_run-0{run}_bold.nii.gz -o {sub_dir}/derivatives/fsl/{task}/run-0{run}/sub-{sub}_ses-{ses:02d}_task-{task}_run-0{run}_bold_spikes.txt --dummy=0'
+            bash_cmd = f'fsl_motion_outliers -i {sub_dir}/func/{sub}_ses-{ses:02d}_task-{task}_run-0{run}_bold.nii.gz -o {sub_dir}/derivatives/fsl/{task}/run-0{run}/{sub}_ses-{ses:02d}_task-{task}_run-0{run}_bold_spikes.txt --dummy=0'
             subprocess.run(bash_cmd.split(),check = True)
             #print(bash_cmd)
 
