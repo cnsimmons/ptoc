d89e4b11a63cff8cc805f4c5c50ffb1756461e71
cnsimmons <claire.simmons112@gmail.com>
2024-08-06 19:09:43 -0400

    func to anat to stand at 1stLevel

--- fmri/pre_proc/zstat2mni.py
+++ fmri/pre_proc/zstat2mni.py
@@ -1,5 +1,6 @@
 """
-Register each 1stlevel to anat in a parallelized manner
+Register each 1stLevel to MNI (anat) in a parallelized manner
+
 """
 curr_dir = f'/user_data/csimmon2/git_repos/ptoc'
 import numpy as np
@@ -7,64 +8,45 @@ import pandas as pd
 import subprocess
 import os
 import pdb
-from nilearn import datasets
-
+from nilearn.datasets import load_mni152_brain_mask, load_mni152_template
 import sys
 sys.path.append(curr_dir)
+
 import ptoc_params as params
 
-
-# Define paths
 data_dir = f'/lab_data/behrmannlab/vlad/hemispace' #hemispace data
-firstlevel_suf = ''
-
-# Load the MNI152 template
-mni_template = datasets.load_mni152_template()
-mni_brain = mni_template
-
 sub_info = pd.read_csv(f'{curr_dir}/sub_info.csv')
-#subs = sub_info[sub_info['group'] == 'control']['sub'].tolist()
-subs = ['sub-025']
+subs = sub_info[sub_info['group'] == 'control']['sub'].tolist()
 task = 'loc'
-#runs = [1,2,3]
-runs = [1]
+runs = [1,2,3]
 cope = 3
 
+mni='/opt/fsl/6.0.3/data/standard/MNI152_T1_2mm_brain.nii.gz' #this is the MNI we use for analysis
+
 for sub in subs:
+    sub_dir = f'{data_dir}/{sub}/ses-01'
     for run in runs:
-    print(f"Processing {sub}")
+        print(f'Registering {sub} run-0{run} to anat and then to MNI')
         
-        # Define paths for this subject
-        sub_dir = os.path.join(data_dir, sub, 'ses-01', 'derivatives', 'fsl', 'loc', 'run-01', '1stLevel.feat')
-        zstat_file = os.path.join(sub_dir, 'stats', 'zstat3.nii.gz')
-        output_file = os.path.join(sub_dir, 'stats', 'zstat3_mni.nii.gz')
+        #register each FirstLevel to anat
+        zstat = f'{sub_dir}/derivatives/fsl/loc/run-0{run}/1stLevel.feat/stats/zstat{cope}.nii.gz'
         
-        # Check if the required files exist
-        if not os.path.exists(zstat_file):
-            print(f"zstat file not found for {sub}")
-            continue
+        # Output paths
+        out_func_anat = f'{sub_dir}/derivatives/fsl/loc/run-0{run}/1stLevel.feat/stats/zstat{cope}_anat.nii.gz'
+        out_func_mni = f'{sub_dir}/derivatives/fsl/loc/run-0{run}/1stLevel.feat/stats/zstat{cope}_mni.nii.gz'
         
-        # Define the transformation matrix
-        transform_mat = os.path.join(sub_dir, 'reg', 'example_func2standard.mat')
-        
-        if not os.path.exists(transform_mat):
-            print(f"Transformation matrix not found for {sub}")
-            continue
-        
-        # Run FLIRT to transform zstat to MNI space
-        cmd = f"flirt -in {zstat_file} -ref {mni_brain} -out {output_file} -applyxfm -init {transform_mat} -interp trilinear"
-        
-        try:
-            subprocess.run(cmd, shell=True, check=True)
-            print(f"Successfully transformed zstat to MNI space for {sub}")
-        except subprocess.CalledProcessError:
-            print(f"Error transforming zstat to MNI space for {sub}")
+        # Reference anatomical image
+        anat_ref = f'{sub_dir}/anat/{sub}_ses-01_T1w_brain.nii.gz'  # Adjust if needed, maybe T1w.nii.gz
 
-    print("Processing complete.")
-
-
-
-
-
-
-
+        if os.path.exists(zstat):
+            # Step 1: Register first-level to anatomical
+            bash_cmd1 = f'flirt -in {zstat} -ref {anat_ref} -out {out_func_anat} -applyxfm -init {sub_dir}/derivatives/fsl/loc/run-0{run}/1stLevel.feat/reg/example_func2highres.mat -interp trilinear'
+            subprocess.run(bash_cmd1.split(), check=True)
+            
+            # Step 2: Register anatomical to MNI
+            bash_cmd2 = f'flirt -in {out_func_anat} -ref {mni} -out {out_func_mni} -applyxfm -init {sub_dir}/anat/anat2stand.mat -interp trilinear'
+            subprocess.run(bash_cmd2.split(), check=True)
+            
+            print(f'Completed registration for {sub} run-0{run}')
+        else:
+            print(f'zstat {zstat} does not exist for subject {sub} run-0{run}')
