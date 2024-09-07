94265a5fe99e5fdb83c9ca0af3a26b3898cb2d8b
cnsimmons <claire.simmons112@gmail.com>
2024-09-06 22:10:37 -0400

    statistics worked

--- analyses/temp.py
+++ analyses/temp.py
@@ -22,15 +22,9 @@ study_dir = f"/lab_data/behrmannlab/vlad/{study}"
 results_dir = '/user_data/csimmon2/git_repos/ptoc/results'
 raw_dir = params.raw_dir
 
-#sub_info = pd.read_csv(f'{curr_dir}/sub_info.csv')
-#sub_info = sub_info[sub_info['group'] == 'control']
-#subs = sub_info['sub'].tolist()
-#subs = ['sub-025','sub-038','sub-057','sub-059','sub-064']  # subs completed, 4.5 hours-ish
-#subs = ['sub-067','sub-068','sub-071'] #subs completed, part 2
-#subs = ['sub-025','sub-038','sub-057','sub-059','sub-064','sub-067','sub-068','sub-071'] #all subs that have run
-#subs = ['sub-083', 'sub-084', 'sub-085', 'sub-087'] #3
-#subs = ['sub-088', 'sub-093', 'sub-094', 'sub-095'] #4
-subs = [ 'sub-096', 'sub-097', 'sub-107'] #5
+sub_info = pd.read_csv(f'{curr_dir}/sub_info.csv')
+sub_info = sub_info[sub_info['group'] == 'control']
+subs = sub_info['sub'].tolist()
 
 rois = ['pIPS', 'LO']
 hemispheres = ['left', 'right']
@@ -220,5 +214,5 @@ def summarize_gca():
 
 # Main execution
 if __name__ == "__main__":
-    conduct_gca()
-    #summarize_gca()
+    #conduct_gca()
+    summarize_gca()
