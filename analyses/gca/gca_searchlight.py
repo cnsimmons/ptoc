04487a0606316f9a884f8a5ad6f45f7e830e00c8
cnsimmons <claire.simmons112@gmail.com>
2024-09-23 21:56:17 -0400

    none of the searchlights are working - start over tomororow

--- analyses/gca/gca_searchlight.py
+++ analyses/gca/gca_searchlight.py
@@ -7,9 +7,11 @@ from statsmodels.tsa.stattools import grangercausalitytests
 import nibabel as nib
 import logging
 import sys
-from brainiak.searchlight.searchlight import Searchlight
+from brainiak.searchlight.searchlight import Searchlight, Ball 
 from mpi4py import MPI
 from sklearn.preprocessing import StandardScaler
+import warnings
+warnings.filterwarnings("ignore")
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -23,6 +25,8 @@ size = comm.Get_size()
 sl_rad = 2 #search light radius
 max_blk_edge = 10 #max block edge
 pool_size = 1
+voxels_proportion = 1
+shape = Ball
 
 # Import your parameters
 curr_dir = f'/user_data/csimmon2/git_repos/ptoc'
@@ -50,7 +54,7 @@ run_combos = [[rn1, rn2] for rn1 in range(1, run_num + 1) for rn2 in range(rn1 +
 
 # Load whole brain mask
 whole_brain_mask = image.load_img('roiParcels/mruczek_parcels/binary/all_visual_areas.nii.gz') 
-mask = whole_brain_mask.get_fdata().astype(bool)
+mask = image.get_data(whole_brain_mask) # the mask to search within 
 
 def extract_roi_sphere(img, coords):
     roi_masker = input_data.NiftiSpheresMasker([tuple(coords)], radius=6)
@@ -188,6 +192,76 @@ def conduct_gca():
                     logging.info(f"Completed Searchlight GCA for {ss}, {tsk}, {seed_roi}_{seed_hemi}")
 
         logging.info(f'Completed Searchlight GCA for subject {ss}')
+
+##NEW##
+
+def load_data():
+    print('Loading data...')
+
+    all_runs = []
+    for run in runs:
+        print(run)
+
+        curr_run = image.load_img(f'{exp_dir}/run-0{run}/1stLevel.feat/filtered_func_data_reg.nii.gz') #load data
+        curr_run = image.get_data(image.clean_img(curr_run,standardize=True,mask_img=whole_brain_mask)) #standardize within mask and convert to numpy
+        curr_run = curr_run[:,:,:,first_fix:] #remove first few fixation volumes
+
+        all_runs.append(curr_run) #append to list
+
+
+        del curr_run
+        print((resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1024)/1024)
+
+    print('data loaded..')
+
+    print('concatenating data..')
+    bold_vol = np.concatenate(np.array(all_runs),axis = 3) #compile into 4D
+    del all_runs
+    print((resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1024)/1024)
+    print('data concatenated...')
+    gc.collect()
+
+    return bold_vol
+
+def extract_seed_ts(bold_vol):
+    """
+    extract all data from seed region
+    """
+
+    #load seed
+    seed_roi = image.get_data(image.load_img(f'{roi_dir}/spheres/{dorsal}_sphere.nii.gz'))
+    reshaped_roi = np.reshape(seed_roi, (91,109,91,1))
+    masked_img = reshaped_roi*bold_vol
+
+    #extract voxel resposnes from within mask
+    seed_ts = masked_img.reshape(-1, bold_vol.shape[3]) #reshape into rows (voxels) x columns (time)
+    seed_ts =seed_ts[~np.all(seed_ts == 0, axis=1)] #remove voxels that are 0 (masked out)
+    seed_ts = np.transpose(seed_ts)
+
+    print('Seed data extracted...')
+
+    return seed_ts
+
+bold_vol = load_data()
+seed_ts = extract_seed_ts(bold_vol)
+
+#run searchlight
+t1 = time.time()
+print("Begin Searchlight", print((resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1024)/1024))
+sl = Searchlight(sl_rad=sl_rad,max_blk_edge=max_blk_edge, shape = shape) #setup the searchlight
+print('Distribute', (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1024)/1024)
+sl.distribute([bold_vol], mask) #send the 4dimg and mask
+
+print('Broadcast', (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1024)/1024)
+sl.broadcast(seed_ts) #send the relevant analysis vars
+print('Run', (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1024)/1024, flush= True)
+sl_result = sl.run_searchlight(mvpd, pool_size=pool_size)
+print("End Searchlight\n", (time.time()-t1)/60)
+
+sl_result = sl_result.astype('double')  # Convert the output into a precision format that can be used by other applications
+sl_result[np.isnan(sl_result)] = 0  # Exchange nans with zero to ensure compatibility with other applications
+sl_nii = nib.Nifti1Image(sl_result, affine)  # create the volume image
+nib.save(sl_nii, f'{out_dir}/{study}{ss}_{dorsal}_searchlight.nii.gz')  # Save the volume
 
 if __name__ == "__main__":
     conduct_gca()
