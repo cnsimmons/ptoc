ea6ed941329c8ab7d79aa9b48d388fb097417cc5
cnsimmons <claire.simmons112@gmail.com>
2024-10-04 13:09:03 -0400

    searchlight

--- analyses/gca/gca_searchlight.py
+++ analyses/gca/gca_searchlight.py
@@ -1,23 +1,22 @@
-import warnings
-warnings.filterwarnings("ignore")
-import resource
-import sys
-import time
 import os
-import gc
 import pandas as pd
 import numpy as np
-import logging
-from nilearn import image, input_data
+from nilearn import image
 from nilearn.glm.first_level import compute_regressor
 from statsmodels.tsa.stattools import grangercausalitytests
+from scipy import stats
+import sys
 import nibabel as nib
+import logging
 from brainiak.searchlight.searchlight import Searchlight, Ball
+from mpi4py import MPI
+import gc
+import time
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Import parameters
+# Import your parameters
 curr_dir = f'/user_data/csimmon2/git_repos/ptoc'
 sys.path.insert(0, curr_dir)
 import ptoc_params as params
@@ -25,44 +24,37 @@ import ptoc_params as params
 # Set up directories and parameters
 study = 'ptoc'
 study_dir = f"/lab_data/behrmannlab/vlad/{study}"
+localizer = 'Object'  # scramble or object. This is the localizer task.
 results_dir = '/user_data/csimmon2/git_repos/ptoc/results'
 raw_dir = params.raw_dir
 
 # Load subject information
 sub_info = pd.read_csv(f'{curr_dir}/sub_info.csv')
 sub_info = sub_info[sub_info['group'] == 'control']
-subs = ['sub-025']  # You can expand this list if needed
+#subs = sub_info['sub'].tolist()
+subs = ['sub-025']
 
-# Other parameters
+run_num = 3
+runs = list(range(1, run_num + 1))
+run_combos = [[rn1, rn2] for rn1 in range(1, run_num + 1) for rn2 in range(rn1 + 1, run_num + 1)]
+
+# Searchlight parameters
+sl_rad = 2
+max_blk_edge = 10
+pool_size = 1
+shape = Ball
+
 rois = ['pIPS', 'LO']
 hemispheres = ['left', 'right']
 run_num = 3
 runs = list(range(1, run_num + 1))
 run_combos = [[rn1, rn2] for rn1 in range(1, run_num + 1) for rn2 in range(rn1 + 1, run_num + 1)]
 
-whole_brain_mask = image.load_img('/user_data/csimmon2/git_repos/ptoc/roiParcels/mruczek_parcels/binary/all_visual_areas.nii.gz')
-affine = whole_brain_mask.affine
-dimsizes = whole_brain_mask.header.get_zooms()  # get dimensions
-
-# scan parameters
-vols = 184
-tr = 2.0
-
-print("Setting up searchlight...")
-mask = image.get_data(whole_brain_mask)  # the mask to search within
-sl_rad = 2  # searchlight radius in voxels
-max_blk_edge = 10  # how many blocks to send on each parallelized search
-pool_size = 1  # how many cores to use
-
-voxels_proportion = 1
-shape = Ball
-
 def extract_roi_sphere(img, coords):
     roi_masker = input_data.NiftiSpheresMasker([tuple(coords)], radius=6)
     seed_time_series = roi_masker.fit_transform(img)
     phys = np.mean(seed_time_series, axis=1).reshape(-1, 1)
-    phys_standardized = phys
-    return phys_standardized
+    return phys  # Return non-standardized time series
 
 def make_psy_cov(runs, ss):
     temp_dir = f'{raw_dir}/{ss}/ses-01'
@@ -74,7 +66,7 @@ def make_psy_cov(runs, ss):
 
     for i, rn in enumerate(runs):
         ss_num = ss.split('-')[1]
-        obj_cov_file = f'{cov_dir}/catloc_{ss_num}_run-0{rn}_Object.txt'
+        obj_cov_file = f'{cov_dir}/catloc_{ss_num}_run-0{rn}_{localizer}.txt'
 
         if not os.path.exists(obj_cov_file):
             logging.warning(f'Covariate file not found for run {rn}')
@@ -97,9 +89,8 @@ def make_psy_cov(runs, ss):
         return np.zeros((total_vols, 1))
 
     psy, _ = compute_regressor(cov.T, 'spm', times)
-    psy[psy > 0] = 1 
-    psy[psy < 0] = 0  
-    
+    psy[psy > 0] = 1
+    psy[psy <= 0] = 0
     return psy
 
 def extract_cond_ts(ts, cov):
@@ -109,74 +100,139 @@ def extract_cond_ts(ts, cov):
     block_ind = (cov == 1).reshape((len(cov))) | block_ind
     return ts[block_ind]
 
-def gca(data, sl_mask, myrad, seed_ts, psy):
-    # This function will be called for each searchlight sphere
-    sphere_ts = data[0]
+def gca_searchlight_kernel(data, sl_mask, myrad, bcvar):
+    sl_data = data[0][sl_mask]
+    sl_data = sl_data.reshape(-1, sl_data.shape[-1]).T
     
-    # Extract condition-specific time series for the sphere and seed ROIs
-    sphere_phys = extract_cond_ts(sphere_ts, psy)
-    dorsal_phys = extract_cond_ts(seed_ts['dorsal'], psy)
-    ventral_phys = extract_cond_ts(seed_ts['ventral'], psy)
+    psy = bcvar
+    sl_data_cond = extract_cond_ts(sl_data, psy)
     
-    # Compute GCA between sphere and seed ROIs
-    neural_ts_dorsal = pd.DataFrame({'sphere': sphere_phys.ravel(), 'dorsal': dorsal_phys.ravel()})
-    neural_ts_ventral = pd.DataFrame({'sphere': sphere_phys.ravel(), 'ventral': ventral_phys.ravel()})
+    n_voxels = sl_data_cond.shape[1]
+    gc_matrix = np.zeros((n_voxels, n_voxels))
     
-    gc_res_dorsal = grangercausalitytests(neural_ts_dorsal, 1, verbose=False)
-    gc_res_ventral = grangercausalitytests(neural_ts_ventral, 1, verbose=False)
+    for i in range(n_voxels):
+        for j in range(i+1, n_voxels):
+            voxel1 = sl_data_cond[:, i]
+            voxel2 = sl_data_cond[:, j]
+            
+            # Check if either time series has very low variance
+            if np.std(voxel1) < 1e-6 or np.std(voxel2) < 1e-6:
+                continue
+            
+            # Check if the time series are highly correlated
+            correlation, _ = stats.pearsonr(voxel1, voxel2)
+            if abs(correlation) > 0.95:  # You can adjust this threshold
+                continue
+            
+            neural_ts = pd.DataFrame({
+                'voxel1': voxel1,
+                'voxel2': voxel2
+            })
+            
+            try:
+                gc_res_1to2 = grangercausalitytests(neural_ts[['voxel1', 'voxel2']], 1, verbose=False)
+                gc_res_2to1 = grangercausalitytests(neural_ts[['voxel2', 'voxel1']], 1, verbose=False)
+                f_diff = gc_res_1to2[1][0]['ssr_ftest'][0] - gc_res_2to1[1][0]['ssr_ftest'][0]
+                gc_matrix[i, j] = f_diff
+                gc_matrix[j, i] = -f_diff
+            except Exception as e:
+                logging.warning(f"Error in GC test: {str(e)}")
+                continue
     
-    f_diff_dorsal = gc_res_dorsal[1][0]['ssr_ftest'][0]
-    f_diff_ventral = gc_res_ventral[1][0]['ssr_ftest'][0]
+    # If all tests failed, return 0
+    if np.all(gc_matrix == 0):
+        return 0
     
-    return np.array([f_diff_dorsal, f_diff_ventral])
+    return np.mean(np.abs(gc_matrix))
 
-def process_subject(ss):
-    logging.info(f'Processing subject {ss}')
-    sub_dir = f'{study_dir}/{ss}/ses-01/'
-    temp_dir = f'{raw_dir}/{ss}/ses-01'
-    roi_dir = f'{sub_dir}/derivatives/rois'
-    exp_dir = f'{temp_dir}/derivatives/fsl/loc'
-    os.makedirs(f'{sub_dir}/derivatives/gca_searchlight', exist_ok=True)
+def conduct_gca_searchlight():
+    logging.info(f'Running GCA Searchlight for {localizer}...')
+    
+    comm = MPI.COMM_WORLD
+    rank = comm.rank
+    size = comm.size
 
-    roi_coords = pd.read_csv(f'{roi_dir}/spheres/sphere_coords_hemisphere.csv')
-
-    for rcn, rc in enumerate(run_combos):
-        logging.info(f"Processing run combination {rc} for subject {ss}")
-        
-        # Load and preprocess data
-        filtered_list = []
-        for rn in rc:
-            curr_run = image.load_img(f'{exp_dir}/run-0{rn}/1stLevel.feat/filtered_func_data_reg.nii.gz')
-            curr_run = image.clean_img(curr_run, standardize=True)
-            filtered_list.append(curr_run)
-
-        img4d = image.concat_imgs(filtered_list)
-        bold_vol = img4d.get_fdata()
-        
-        # Extract ROI time series
-        dorsal_ts = extract_roi_sphere(img4d, roi_coords[(roi_coords['roi'] == 'pIPS') & (roi_coords['hemisphere'] == 'left')][['x', 'y', 'z']].values.tolist()[0])
-        ventral_ts = extract_roi_sphere(img4d, roi_coords[(roi_coords['roi'] == 'LO') & (roi_coords['hemisphere'] == 'left')][['x', 'y', 'z']].values.tolist()[0])
-        
-        psy = make_psy_cov(rc, ss)
-        
-        seed_ts = {'dorsal': dorsal_ts, 'ventral': ventral_ts}
-        
-        # Set up and run searchlight
-        sl = Searchlight(sl_rad=sl_rad, max_blk_edge=max_blk_edge, shape=shape)
-        sl.distribute([bold_vol], mask)
-        sl.broadcast(seed_ts)
-        sl.broadcast(psy)  # Broadcast psy to all processes
-        
-        # Modify the gca function call to include psy
-        sl_result = sl.run_searchlight(gca, pool_size=pool_size)
-        
-        # Save results
-        result_img = nib.Nifti1Image(sl_result, affine)
-        nib.save(result_img, f'{sub_dir}/derivatives/gca_searchlight/gca_searchlight_result_rc{rcn}.nii.gz')
-
-    logging.info(f'Completed processing for subject {ss}')
-
-# Main execution
-if __name__ == "__main__":
     for ss in subs:
-        process_subject(ss)
+        sub_dir = f'{study_dir}/{ss}/ses-01/'
+        temp_dir = f'{raw_dir}/{ss}/ses-01'
+        exp_dir = f'{temp_dir}/derivatives/fsl/loc'
+        os.makedirs(f'{sub_dir}/derivatives/gca_searchlight', exist_ok=True)
+
+        for rcn, rc in enumerate(run_combos):
+            logging.info(f"Processing run combination {rc} for subject {ss}")
+            
+            filtered_list = []
+            for rn in rc:
+                curr_run = image.load_img(f'{exp_dir}/run-0{rn}/1stLevel.feat/filtered_func_data_reg.nii.gz')
+                curr_run = image.clean_img(curr_run, standardize='zscore_sample')
+                filtered_list.append(curr_run)
+
+            img4d = image.concat_imgs(filtered_list)
+            data = img4d.get_fdata()
+            
+            mask = (np.sum(data, axis=-1) != 0).astype(int)
+
+            psy = make_psy_cov(rc, ss)
+            
+            logging.info(f"Data shape: {data.shape}")
+            logging.info(f"Mask shape: {mask.shape}")
+            logging.info(f"Psychological covariate shape: {psy.shape}")
+            
+            sl = Searchlight(sl_rad=sl_rad, max_blk_edge=max_blk_edge, shape=shape)
+            sl.distribute([data], mask)
+            sl.broadcast(psy)
+
+            t1 = time.time()
+            sl_result = sl.run_searchlight(gca_searchlight_kernel, pool_size=pool_size)
+            logging.info(f"Searchlight duration: {(time.time()-t1)/60} minutes")
+
+            if rank == 0:
+                sl_result = sl_result.astype('double')
+                sl_result[np.isnan(sl_result)] = 0
+                logging.info(f"Searchlight result shape: {sl_result.shape}")
+                logging.info(f"Searchlight result min: {np.min(sl_result)}, max: {np.max(sl_result)}")
+                logging.info(f"Number of non-zero voxels: {np.sum(sl_result != 0)}")
+                sl_img = nib.Nifti1Image(sl_result, img4d.affine)
+                nib.save(sl_img, f'{sub_dir}/derivatives/gca_searchlight/gca_searchlight_result_rc{rcn+1}.nii.gz')
+                logging.info(f"Saved searchlight results for subject {ss}, run combo {rcn+1}")
+
+            gc.collect()
+
+        logging.info(f'Completed GCA Searchlight for subject {ss}')
+
+def summarize_gca_searchlight():
+    logging.info('Creating summary across subjects...')
+    
+    all_subjects_data = []
+    
+    for ss in subs:
+        sub_dir = f'{study_dir}/{ss}/ses-01/'
+        data_dir = f'{sub_dir}/derivatives/gca_searchlight'
+        
+        for rcn in range(len(run_combos)):
+            result_file = f'{data_dir}/gca_searchlight_result_rc{rcn+1}.nii.gz'
+            if os.path.exists(result_file):
+                img = nib.load(result_file)
+                data = img.get_fdata()
+                all_subjects_data.append(data)
+    
+    if all_subjects_data:
+        mean_data = np.mean(all_subjects_data, axis=0)
+        std_data = np.std(all_subjects_data, axis=0)
+        
+        output_dir = f"{results_dir}/gca_searchlight"
+        os.makedirs(output_dir, exist_ok=True)
+        
+        mean_img = nib.Nifti1Image(mean_data, img.affine)
+        std_img = nib.Nifti1Image(std_data, img.affine)
+        
+        nib.save(mean_img, f"{output_dir}/all_subjects_gca_searchlight_mean_{localizer.lower()}.nii.gz")
+        nib.save(std_img, f"{output_dir}/all_subjects_gca_searchlight_std_{localizer.lower()}.nii.gz")
+        
+        logging.info(f'Summary across subjects completed and saved to {output_dir}')
+    else:
+        logging.error("No valid data found for any subjects. Cannot create summary.")
+
+if __name__ == "__main__":
+    conduct_gca_searchlight()
+    #summarize_gca_searchlight()
