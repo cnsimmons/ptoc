a330c0ad545d9601769260f9fadd57a9aed5a143
cnsimmons <claire.simmons112@gmail.com>
2024-11-20 18:34:14 -0500

    reran matrices and continued file cleaning

--- ptoc_params.py
+++ ptoc_params.py
@@ -12,18 +12,18 @@ import subprocess
 from glob import glob as glob
 import pdb
 
-suf = ''
+suf = 'toolloc'
 
 #runs = [1,2,3]
 runs = [1,2] #change to 2 for sub_057
 raw_dir = '/lab_data/behrmannlab/vlad/hemispace'
 data_dir = '/lab_data/behrmannlab/vlad/ptoc'
-results_dir = '/user_data/csimmon2/git_repos/ptoc/results/{task}'
+results_dir = '/user_data/csimmon2/git_repos/ptoc/results/tools'
 fig_dir = '/user_data/csimmon2/git_repos/ptoc/results/figures'
 sub_info = pd.read_csv(f'{curr_dir}/sub_info.csv')
 sub_info_tool = pd.read_csv(f'{curr_dir}/sub_info_tool.csv')
 sub_info_OTC = pd.read_csv(f'{curr_dir}/sub_info_OTC.csv')
-task_info = pd.read_csv(f'/user_data/csimmon2/git_repos/ptoc/task_info{suf}.csv')
+task_info = pd.read_csv(f'/user_data/csimmon2/git_repos/ptoc/task_info.csv')
 
 hemis = ['left','right']
 #rois = ['ventral_visual_cortex', 'dorsal_visual_cortex', 'LO', 'PFS', 'pIPS','aIPS', 'V1'] #adding V1 for early vision scramble and full hemispace || I need to add 'hemi' but at the moment I can't find the roi file so I'm leaving as is with just the added V1
